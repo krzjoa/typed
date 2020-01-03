b7193e379507e8438ececd4f9b5a8e930254f739
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2020-01-03 09:57:08 +0100

    Update locked.R

--- R/locked.R
+++ R/locked.R
@@ -1,22 +1,21 @@
 #' @name assign
 #' @title Typed assign
 #' @description Drop-in replacement for standard assignment to parse input
-#'
+#' TODO: limit function to executing pre.hook and post.hook only
 `<-` <- function(x, value){
+  
   # Split symbol into function and call
-  assign('ref.env', parent.frame())
-  assign('passed.call', rlang::enexpr(x))
+  .Primitive("<-")('ref.env', parent.frame())
+  .Primitive("<-")('passed.call', rlang::enexpr(x))
 
   .Primitive("<-")(fun.name, NULL)
-  .Primitive("<-")(var.name, NULL)
+  # .Primitive("<-")(var.name, NULL)
 
   if (is.call(passed.call)) {
     .Primitive("<-")(fun.name, rlang::call_name(passed.call))
     .Primitive("<-")(fun.args, rlang::call_args(passed.call))
     .Primitive("<-")(var.name, deparse(fun.args[[1]]))
     .Primitive("<-")(rest.args, fun.args[-1])
-
-    # browser()
 
     if (exists(var.name, envir = ref.env)){
       .Primitive("<-")(variable, get(var.name, envir = ref.env))
@@ -32,76 +31,50 @@
   } else {
     .Primitive("<-")(var.name, deparse(substitute(x)))
   }
+  
+  # Pre hook 
+  # TODO
 
   # Assignment
   do.call(.Primitive("<-"), list(var.name, value), envir = ref.env)
+  
+  # Post hook
+  if (!is.null(attr(fun.name, 'post.hook')))
+    attr(fun.name, 'pre.hook')(var.name, ..., env = ref.name)
 
-  if (!is.null(fun.name))
-    if (any(fun.name %in% c('locked', 'immutable')))
-      lockBinding(var.name, env = ref.env)
 }
-
-z <- 10
-locked(q) <- 20
-q <- 10
-
-123 -> locked(ert)
-ert <- 25
-
 
 `at<-` <- function(a, b, c){
   a[b[[1]], b[[2]]] <- c
   a
 }
 
+# colnames(z) <- c(1,2,3)
 mat <- matrix(0, 3, 3)
-
 mat[2, 2] <- 7
-
 at(mat, c(1,2)) <- 5
 
-
-immutable(super.variable) <- 789
-
 # # rm(`<-`)
-#
-# c(locked, integer) : x <- 10
-# casted(integer)
-#
-#
-#
-# typed_function <- function(...){
-#   raw.exprs <- rlang::exprs(...)
-#   function(function.body, output){
-#
-#   }
-# }
-#
-# qwer <- typed_function(integer: x, char: y = "trolo")({
-#
-# }, integer)
-#
-#
-# qwer <- function(x = typed(integer), y = typed(char), z = typed(data.frame)){
-#
-# }
 
-
+#' @name locked
+#' @title Lock variable
+#' @examples
+#' x <- 10
+#' x <- 2.5
+#' x
+#' locked(q) <- 20
+#' q <- 10
+#' 123 -> locked(y)
+#' y <- 25
 `locked<-` <- function(x, value){
-  # browser()
-  #assign(substitute(x), value = value, envir = parent.frame())
-  #lockBinding(x, parent.frame())
   value
 }
 
-locked(x) <- 5
+# Post hook
+attr(`locked<-`, 'typed.post.hook') <- function(x, ..., env){
+   lockBinding(var.name, env = env)
+}
 
-# x <- locked(5)
-#
-# colnames(z) <- c(1,2,3)
-#
-# assign(substitute(x))
-# ?base::`<-`
-#
-# qwer <- list()
-
+#' @rdname locked
+#' @export
+`immutable<-` <- `locked<-`
