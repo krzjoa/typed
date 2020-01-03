da286b67780a342b93d20607353cdb8a03888f31
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2020-01-03 09:48:57 +0100

    Update typed.R

--- R/typed.R
+++ R/typed.R
@@ -1,1 +1,21 @@
 
+# c(locked, integer) : x <- 10
+# casted(integer)
+#
+#
+#
+# typed_function <- function(...){
+#   raw.exprs <- rlang::exprs(...)
+#   function(function.body, output){
+#
+#   }
+# }
+#
+# qwer <- typed_function(integer: x, char: y = "trolo")({
+#
+# }, integer)
+#
+#
+# qwer <- function(x = typed(integer), y = typed(char), z = typed(data.frame)){
+#
+# }
